ad90720d18756ab649854f9421b0e872af0d67c3
srikanth mutyala <sriki-duplicate@users.noreply.github.com>
2017-11-26 11:38:51 -0600

    Add files via upload

--- PID.cpp
+++ PID.cpp
@@ -1,4 +1,6 @@
 #include "PID.h"
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
@@ -11,89 +13,94 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	this->Kp = Kp;
-	this->Ki = Ki;
-	this->Kd = Kd;
-	p_error = 0;
-	i_error = 0;
-	d_error = 0;
-	isTwiddle = true;
-	isAdd = false;
-	isSub = false;
-	//p = { Kp, Ki, Kd };
-	p = { 0, 0, 0 };
-	dp = { 1, 1, 1 };
+	PID::Kp = Kp;
+	PID::Ki = Ki;
+	PID::Kd = Kd;
+	p_error = d_error = i_error = 0.0;
+
+	// Twiddling parameters
+	yes_i_wanna_twiddle = false;
+	dp = { 0.1*Kp,0.1*Kd,0.1*Ki };
 	step = 1;
-	settling_steps = 100;
-	eval_steps = 500;
-	err = 0;
+	param_index = 2;  // this will wrao back to 0 after the first twiddle loop
+	n_settle_steps = 100;
+	n_eval_steps = 2000;
+	total_error = 0;
 	best_error = std::numeric_limits<double>::max();
-	thresh = 0.001;
+	tried_adding = false;
+	tried_subtracting = false;
 }
 
 void PID::UpdateError(double cte) {
-	//Setting p_error to cte on step 1 here so the d_error doesn't start off big
-	if (step == 1)
+	if (step == 1) {
+		// to get correct initial d_error
 		p_error = cte;
-
-	//Setting the errors
+	}
 	d_error = cte - p_error;
 	p_error = cte;
 	i_error += cte;
-	
-	//Based on the Parameter Optimization lecture video, we are using a settling period (100 steps) before we 
-	//update the total CTE
-	if (step % (settling_steps+eval_steps) > settling_steps)
-		err += pow(cte, 2);
 
-
-	//To arrive at optimized parameters, use Twiddle
-	if (isTwiddle && step % (settling_steps + eval_steps) == 0)
-	{
-		dp_sum = std::accumulate(dp.begin(), dp.end(), 0.0);
-		int dp_size = dp.size()
-		while (dp_sum > thresh)
-		{
-			for (size_t i = 0; i < dp; i++)
-			{
-				if (err < best_error)
-				{
-					best_error = err;
-					if (step != settling_steps + eval_steps)
-					{
-						dp[i] *= 1.1;
-					}
-					isAdd = false;
-					isSub - false;
-				}
-				
-				if (!isAdd && !isSub)
-				{
-					p[i] += dp[i];
-					isAdd = true;
-				}
-				else if (isAdd && !isSub)
-				{
-					p[i] -= 2*dp[i];
-					isSub = true;
-				}
-				else
-				{
-					p[i] += dp[i];
-					dp[i] *= 0.9;
-					isAdd = false;
-					isSub = false;
-				}
-			}
-			err = 0;
-			cout << "new parameters" << endl;
-			cout << "P: " << p[0] << ", I: " << p[1] << ", D: " << p[2] << endl;
-		}
+	// update total error only if we're past number of settle steps
+	if (step % (n_settle_steps + n_eval_steps) > n_settle_steps) {
+		total_error += pow(cte, 2);
 	}
-	
+
+	// last step in twiddle loop... twiddle it?
+	if (yes_i_wanna_twiddle && step % (n_settle_steps + n_eval_steps) == 0) {
+		cout << "step: " << step << endl;
+		cout << "total error: " << total_error << endl;
+		cout << "best error: " << best_error << endl;
+		if (total_error < best_error) {
+			cout << "improvement!" << endl;
+			best_error = total_error;
+			if (step != n_settle_steps + n_eval_steps) {
+				// don't do this if it's the first time through
+				dp[param_index] *= 1.1;
+			}
+			// next parameter
+			param_index = (param_index + 1) % 3;
+			tried_adding = tried_subtracting = false;
+		}
+		if (!tried_adding && !tried_subtracting) {
+			// try adding dp[i] to params[i]
+			AddToParameterAtIndex(param_index, dp[param_index]);
+			tried_adding = true;
+		}
+		else if (tried_adding && !tried_subtracting) {
+			// try subtracting dp[i] from params[i]
+			AddToParameterAtIndex(param_index, -2 * dp[param_index]);
+			tried_subtracting = true;
+		}
+		else {
+			// set it back, reduce dp[i], move on to next parameter
+			AddToParameterAtIndex(param_index, dp[param_index]);
+			dp[param_index] *= 0.9;
+			// next parameter
+			param_index = (param_index + 1) % 3;
+			tried_adding = tried_subtracting = false;
+		}
+		total_error = 0;
+		cout << "new parameters" << endl;
+		cout << "P: " << Kp << ", I: " << Ki << ", D: " << Kd << endl;
+	}
+	step++;
 }
 
 double PID::TotalError() {
-	return -Kp * p_error - Kd * d_error - Ki * i_error;
+	return 0.0;  // ?
 }
 
+void PID::AddToParameterAtIndex(int index, double amount) {
+	if (index == 0) {
+		Kp += amount;
+	}
+	else if (index == 1) {
+		Kd += amount;
+	}
+	else if (index == 2) {
+		Ki += amount;
+	}
+	else {
+		std::cout << "AddToParameterAtIndex: index out of bounds";
+	}
+}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "json.hpp"
 #include "PID.h"
 #include <math.h>
+#include <limits>
 
 // for convenience
 using json = nlohmann::json;
@@ -16,117 +17,139 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
 std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_last_of("]");
-  if (found_null != std::string::npos) {
-    return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
+	auto found_null = s.find("null");
+	auto b1 = s.find_first_of("[");
+	auto b2 = s.find_last_of("]");
+	if (found_null != std::string::npos) {
+		return "";
+	}
+	else if (b1 != std::string::npos && b2 != std::string::npos) {
+		return s.substr(b1, b2 - b1 + 1);
+	}
+	return "";
 }
 
 int main()
 {
-  uWS::Hub h;
+	uWS::Hub h;
 
-  PID pid, pid_s, pid_t;
-  // TODO: Initialize the pid variable.
-  //pid.Init(0, 0, 0);
-  pid.Init(0.2, 0.004, 3.0);
-  //pid.Init(0.185, 0.003, 3.5);
-  //pid_s.Init(0.134611, 0.000270736, 3.05349);
-  //pid_t.Init(0.316731, 0.0000, 0.0226185);
+	// steering and throttle PID controllers
+	PID pid_s, pid_t;
+	// TODO: Initialize the pid variable.
+	//pid.Init(0.03, 0.5, 0.10);
 
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
-      auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
-        auto j = json::parse(s);
-        std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-		  pid.UpdateError(cte);
-		  steer_value = -pid.Kp * pid.p_error - pid.Kd * pid.d_error - pid.Ki * pid.i_error;
+	//pid_s.Init(0.085, 0.0003, 1.35);
+	//pid_t.Init(0.2, 0.0000, 0.01);
 
-		  //steer_value = pid.TotalError();
+	// twiddle update - error: 803.649
+	//pid_s.Init(0.134611 0.000276054, 3.0903);
+	//pid_t.Init(0.316731, 0.0000, 0.0228911);
 
-		  //pid_s.UpdateError(cte);
-		  //steer_value = -pid_s.Kp * pid_s.p_error - pid_s.Kd * pid_s.d_error - pid_s.Ki * pid_s.i_error;
+	// twiddle update - error: 782.494 - max throttle 0.7
+	// pid_s.Init(0.134611, 0.000270736, 3.05349);
+	// pid_t.Init(0.316731, 0.0000, 0.0226185);
 
-		  //pid_t.UpdateError(fabs(cte));
-		  //throttle_value = 0.75 - pid_t.Kp * pid_t.p_error - pid_t.Kd * pid_t.d_error - pid_t.Ki * pid_t.i_error;
-		  ////throttle_value = 0.80 - pid_t.Kp * pid_t.p_error - pid_t.Kd * pid_t.d_error - pid_t.Ki * pid_t.i_error;
+	// twiddle update - error: ?? - max throttle 0.8
+	pid_s.Init(0.134611, 0.000270736, 3.05349);
+	pid_t.Init(0.316731, 0.0000, 0.0226185);
 
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+	h.onMessage([&pid_s, &pid_t](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+		// "42" at the start of the message means there's a websocket message event.
+		// The 4 signifies a websocket message
+		// The 2 signifies a websocket event
+		if (length && length > 2 && data[0] == '4' && data[1] == '2')
+		{
+			auto s = hasData(std::string(data).substr(0, length));
+			if (s != "") {
+				auto j = json::parse(s);
+				std::string event = j[0].get<std::string>();
+				if (event == "telemetry") {
+					// j[1] is the data JSON object
+					double cte = std::stod(j[1]["cte"].get<std::string>());
+					double speed = std::stod(j[1]["speed"].get<std::string>());
+					double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+					double steer_value, throttle_value;
+					/*
+					* TODO: Calcuate steering value here, remember the steering value is
+					* [-1, 1].
+					* NOTE: Feel free to play around with the throttle and speed. Maybe use
+					* another PID controller to control the speed!
+					*/
 
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-		  msgJson["throttle"] = 0.3; throttle_value; // 0.3;
-		  //msgJson["throttle"] = (1 - std::abs(steer_value)) * 0.5 + 0.2;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
-          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-        }
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-      }
-    }
-  });
+					// update error and calculate steer_value at each step
+					pid_s.UpdateError(cte);
+					steer_value = -pid_s.Kp * pid_s.p_error
+						- pid_s.Kd * pid_s.d_error
+						- pid_s.Ki * pid_s.i_error;
 
-  // We don't need this since we're not using HTTP but if it's removed the program
-  // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-    const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1)
-    {
-      res->end(s.data(), s.length());
-    }
-    else
-    {
-      // i guess this should be done more gracefully?
-      res->end(nullptr, 0);
-    }
-  });
+					// update error and calculate throttle_value at each step
+					pid_t.UpdateError(fabs(cte));     // |cte|
+													  //pid_t.UpdateError(pow(cte, 2));   // cte^2
+					throttle_value = 0.75 - pid_t.Kp * pid_t.p_error
+						- pid_t.Kd * pid_t.d_error
+						- pid_t.Ki * pid_t.i_error;
 
-  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
-  });
+					// DEBUG
+					/*
+					std::cout << "Steer value breakdown: " << std::endl;
+					std::cout << "P: " << - pid_s.Kp * pid_s.p_error << std::endl;
+					std::cout << "D: " << - pid_s.Kd * pid_s.d_error << std::endl;
+					std::cout << "I: " << - pid_s.Ki * pid_s.i_error << std::endl;
+					*/
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-    ws.close();
-    std::cout << "Disconnected" << std::endl;
-  });
+					// DEBUG
+					//std::cout << "CTE: " << cte << " Steering Value: " << steer_value << " Throttle Value: " << //throttle_value << std::endl;
 
-  int port = 4567;
-  if (h.listen(port))
-  {
-    std::cout << "Listening to port " << port << std::endl;
-  }
-  else
-  {
-    std::cerr << "Failed to listen to port" << std::endl;
-    return -1;
-  }
-  h.run();
+					json msgJson;
+					msgJson["steering_angle"] = steer_value;
+					msgJson["throttle"] = throttle_value;
+					auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+					//std::cout << msg << std::endl;
+					ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+				}
+			}
+			else {
+				// Manual driving
+				std::string msg = "42[\"manual\",{}]";
+				ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+			}
+		}
+	});
+
+	// We don't need this since we're not using HTTP but if it's removed the program
+	// doesn't compile :-(
+	h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+		const std::string s = "<h1>Hello world!</h1>";
+		if (req.getUrl().valueLength == 1)
+		{
+			res->end(s.data(), s.length());
+		}
+		else
+		{
+			// i guess this should be done more gracefully?
+			res->end(nullptr, 0);
+		}
+	});
+
+	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+		std::cout << "Connected!!!" << std::endl;
+	});
+
+	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
+		ws.close();
+		std::cout << "Disconnected" << std::endl;
+	});
+
+	int port = 4567;
+	if (h.listen(port))
+	{
+		std::cout << "Listening to port " << port << std::endl;
+	}
+	else
+	{
+		std::cerr << "Failed to listen to port" << std::endl;
+		return -1;
+	}
+	h.run();
 }
